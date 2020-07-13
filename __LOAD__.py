668446e76f1746af813268d92a604c8400f6aa8d
Rafael <rafaelramirezq0@gmail.com>
2020-07-13 16:38:17 -0500

    prueba

---
+++ __LOAD__.py
@@ -0,0 +1,19 @@
+import pygame, sys
+
+# Sprites Player
+run1=pygame.image.load("Sprites/dog/run1.png")
+run2=pygame.image.load("Sprites/dog/run2.png")
+run3=pygame.image.load("Sprites/dog/run3.png")
+run4=pygame.image.load("Sprites/dog/run4.png")
+run5=pygame.image.load("Sprites/dog/run5.png")
+run6=pygame.image.load("Sprites/dog/run6.png")
+run7=pygame.image.load("Sprites/dog/run7.png")
+attack=pygame.image.load("Sprites/dog/atack.png")
+run1i=pygame.transform.flip(run1,True,False)
+run2i=pygame.transform.flip(run2,True,False)
+run3i=pygame.transform.flip(run3,True,False)
+run4i=pygame.transform.flip(run4,True,False)
+run5i=pygame.transform.flip(run5,True,False)
+run6i=pygame.transform.flip(run6,True,False)
+run7i=pygame.transform.flip(run7,True,False)
+attack_inv=pygame.transform.flip(attack,True,False)
